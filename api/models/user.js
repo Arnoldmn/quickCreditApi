18c18199f3e42606910204337ac037647ed145a9
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-04-30 12:35:24 +0300

    added user file

--- api/models/user.js
+++ api/models/user.js
@@ -1,2 +1,40 @@
 const express = require('express');
+const db = require('./db')
 const app = express();
+
+const User = db.model('User', new db.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        reqiured: true,
+    },
+    status: {
+        type: String,
+    },
+    isAdmin: {
+        type: boolean,
+        reqiured: true
+    }
+
+
+
+}));
+
