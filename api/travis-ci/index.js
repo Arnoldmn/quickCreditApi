348a73e5b9a148d693190c98560bbf88b3b2186e
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-04-30 11:45:09 +0300

    added models folder

--- api/travis-ci/index.js
+++ api/travis-ci/index.js
@@ -5,6 +5,18 @@ app.get('/', function (res, req) {
     res.send('Hello world');
 })
 
+app.get('/api/v1/signup', function () {
+
+});
+
+app.get('/api/v1/signin', function () {
+
+});
+
+app.get('./api/v1/signin:id', function () {
+
+});
+
 const server = app.listen(3000, function () {
     console.log('App running on port 3000');
 });
